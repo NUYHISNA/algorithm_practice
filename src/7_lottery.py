09dccc34aa4cbfb141390848cc1018cb0a896e63
rlaalswo04 <bravegirls1234@gmail.com>
2023-08-12 11:51:46 +0900

    로또 번호 출력

---
+++ src/7_lottery.py
@@ -0,0 +1,35 @@
+# my_values = []
+# my_values.append(1)
+# my_values.append(2)
+
+# for value in my_values:
+#     print(value)
+
+# match_value = 2
+
+# if match_value in my_values:
+#     print('there exists value')
+# else:
+#     print('there exists no value')
+
+
+# import random
+
+# lotto_numbers = []
+
+# for _ in range(6):
+#     value = random.randint(1, 50)
+#     if value in lotto_numbers:
+#         continue
+#     lotto_numbers.append(value)
+lotto_numbers = []
+import random
+while True:
+    value = random.randint(1, 50)
+    if value in lotto_numbers:
+        continue
+    lotto_numbers.append(value)
+    if len(lotto_numbers) == 6:
+        break
+print(lotto_numbers)
+
