6800852055d2a892f142a9fa5e00da5d33446586
NUYHISNA <skt37525117@gmail.com>
2023-09-16 11:53:50 +0900

    고양이 클래스 아픔&배고픔 기능 추가

--- src/10_class_practice/cat.py
+++ src/10_class_practice/cat.py
@@ -1,3 +1,5 @@
+import random
+
 class Cat:
     def __init__(self, cat_name, cat_age, cat_color, cat_favoritefood, cat_favoritetoy, cat_cham):
         self.name = cat_name
@@ -6,7 +8,7 @@ class Cat:
         self.favoritefood = cat_favoritefood
         self.favoritetoy = cat_favoritetoy
         self.cham = cat_cham
-        self.needed_food_range = 5
+        self.needed_food_range = 10
         self.weight = 5
         self.sick = False
         
@@ -18,3 +20,23 @@ class Cat:
     def notice_sick(self):
         if self.sick == True:
             print("아픔")   
+            
+    def changed_sick_status(self):
+        sick_value = random.randint(0, 1)
+        print(sick_value)
+        if sick_value == 0:
+            self.sick = True
+        else:
+            self.sick = False
+            
+    def cat_caring(self):
+        print(f"{self.name}를 돌보시오.")
+        self.sick = False
+        
+    def cat_feed(self):
+        self.needed_food_range -= 1
+        if self.needed_food_range == -15:
+            print("죽음")
+        elif self.needed_food_range == 4:
+            print("배고파요 :(")
+        
--- src/10_class_practice/main.py
+++ src/10_class_practice/main.py
@@ -1,4 +1,6 @@
 import cat
+import schedule
+import time
 
 my_cat = cat.Cat("Kiki", "3_year_old", "black", "food_fish", "toy_mouth", "haggard")
 print(my_cat.name)
@@ -10,3 +12,12 @@ my_cat.feed()
 print(f"{my_cat.name} 배고픈 정도: {my_cat.needed_food_range}")
 print(f"{my_cat.name} 몸무게: {my_cat.weight}")
 
+def hello_print():
+    print("Hello!!")
+    
+schedule.every(1).weeks.do(my_cat.changed_sick_status)
+schedule.every(5).seconds.do(my_cat.cat_feed)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
